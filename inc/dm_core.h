fd26faafc27621c6ad4489711760ff2cc2a282eb
drdronka <dronka@runbox.com>
2025-07-11 02:32:51 +0200

    minor changes

--- inc/dm_core.h
+++ inc/dm_core.h
@@ -17,18 +17,18 @@ class dm_core
         void cmd_loop_start();
         void cmd_loop_stop();
         void cmd_loop(); // started by dm_core_cmd_loop(), not to be used directly
-        
+
+    protected:
+        bool process_debug_event(DEBUG_EVENT* event, CREATE_PROCESS_DEBUG_INFO* proc_debug_info);
         void proc_show_list();
         void proc_run(char const* const path);
         void proc_attach(UINT32 pid);
         void proc_stop();
         void proc_start();
 
-    protected:
-        bool process_debug_event(DEBUG_EVENT* event, CREATE_PROCESS_DEBUG_INFO* proc_debug_info);
-
         HANDLE proc_handle;
         CREATE_PROCESS_DEBUG_INFO proc_debug_info;
+        bool attached;
 
         dm_log* log;
         dm_scan* scan;
@@ -37,7 +37,6 @@ class dm_core
 
         HANDLE cmd_thread;
         DWORD cmd_thread_id;
-        bool attached;
 
         static char const debug_event_id_name[][27];
 };
